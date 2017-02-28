27010092bf3bb0c509faddb24fc5776a03d733e2
m-strasser <mst1409@gmx.at>
2017-02-28 19:27:05 +0100

    Added parsing of config file.

--- email_from_template.py
+++ email_from_template.py
@@ -2,6 +2,7 @@
 Sets up a simple flask site that allows to render an HTML template and mail it
 to a given e-mail-address.
 """
+import yaml
 from flask import Flask
 
 #### SERVER SIDE #################
@@ -11,16 +12,33 @@ from flask import Flask
 ##################################
 
 #### CLIENT SIDE #################
-# Show inputs for allowed tags
+# Show input form for allowed tags
 # Accept input
 # Call backend
 ##################################
 
 app = Flask(__name__)
 
-@app.route("/")
-def show_template():
-    return "TEMPLATE"
+def read_config(path):
+    """
+    Reads the configuration file from the given path.
+    :param path: The path to the configuration file (a YAML file).
+    :returns: The parsed configuration.
+    :raises IOError: If the file at the given path does not exist.
+    """
+    if not os.path.isfile(path):
+        raise IOError('Could not find config file {}'.format(path))
+
+    with open(path, 'r') as configfile:
+        return yaml.load(configfile)
+
+@app.route("/", methods=['GET'])
+def show_input_form():
+    """
+    Displays the input form which allows a user to enter the values of the
+    corresponding tags.
+    """
+    return render_template('u')
 
 if __name__ == '__main__':
     app.run()
