72a12f32b5ed388f3b6e777abfbd101069e36e1f
m-strasser <mst1409@gmx.at>
2017-03-01 09:57:15 +0100

    Implemented injection of email field

--- email_from_template.py
+++ email_from_template.py
@@ -2,6 +2,7 @@
 Sets up a simple flask site that allows to render an HTML template and mail it
 to a given e-mail-address.
 """
+import logging
 import os
 import yaml
 import re
@@ -39,6 +40,29 @@ def read_tags_from_template(name):
         os.path.dirname(os.path.abspath(__file__)), name), 'r') as template:
         map(lambda x: tags.extend(regex.findall(x)), template.readlines())
 
+def inject_email_field(action, method, tmpl, **context):
+    """
+    Renders a template, then injects an email input field right after the body
+    tag.
+    :param action: The value for the forms action tag (i.e. a URL).
+    :param method: The value for the forms method tag (i.e. POST or GET).
+    :param tmpl: The name of the template.
+    :param context: The context of the template (i.e. the values you want to
+    pass to it).
+    :returns: The rendered template with the injected field.
+    """
+    injection = """
+    <form action='""" + action + """' method='""" + method + """'>
+        <label for="email" id="gen_email_lbl">Your E-mail address:</label>
+        <input type="email" name="email" id="gen_email_field"/>
+        <input type="submit" value="Send" id="gen_submit"/>
+    </form>
+    """
+    rendered = render_template(tmpl, **context).split('\n')
+    body_index = next(i for i,x in enumerate(rendered) if '<body>' in x)
+    rendered.insert(body_index+1, injection)
+    return '\n'.join(rendered)
+
 @app.route("/", methods=['GET'])
 def show_input_form():
     """
@@ -47,13 +71,23 @@ def show_input_form():
     """
     return render_template('input_form.html', fields=tags)
 
+@app.route("/preview_email", methods=['GET', 'POST'])
+def preview_email():
+    if request.method == 'GET':
+        return redirect('/')
+
+    # Just pass those tags that we extracted earlier, you never know
+    tmpl = inject_email_field(url_for('render_email'), 'POST',
+            config['template'],
+            **{k: v for k, v in request.form.iteritems() if k in tags})
+
+    return tmpl
+
 @app.route("/render_email", methods=['GET', 'POST'])
 def render_email():
     if request.method == 'GET':
         return redirect('/')
-    # Just pass those tags that we extracted earlier, you never know
-    return render_template(config['template'],
-           **{k: v for k, v in request.form.iteritems() if k in tags})
+    return render_template('render_email.html', success=False)
 
 def main():
     """
